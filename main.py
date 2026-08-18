cc6c204ea8ae6891ee3e2fa7726cbf1cef38128b
aliyevbek11-lab <aliyevbek11@gmail.com>
2026-08-18 16:27:20 +0300

    Update main.py

--- main.py
+++ main.py
@@ -190,8 +190,7 @@ async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
         return
 
     user_id = update.effective_user.id
-    text = update.message.text.strip()
-
+    text = (update.message.text or "").strip()
     # MENU TUGMALARI
     if text == "🛠 Usta chaqirish":
         await start_order(update, context)
